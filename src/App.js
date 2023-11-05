bf4d0b15d9f77864d6ed343ce05dbd5088a327a5
Kaarish Parameswaran <kingkaarish@gmail.com>
2023-11-04 20:31:40 -0400

    commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+  <div className="center-text">
+    <img src={logo} className="App-logo" alt="logo" />
+    <h1>Welcome to FullStack Development - I</h1>
+    <h2>React JS Programming Week09 Lab Exercise</h2>
+    <h3>101355699</h3>
+    <h3>Kaarish Parameswaran</h3>
+    <h3>George Brown College, Toronto</h3>
+  </div>
+  )     
+}
+
+export default App;
